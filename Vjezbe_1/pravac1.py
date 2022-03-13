a5189e38798509203fc2665c998b5dac5f0a2683
Drago <dmihaljev@pmfst.hr>
2022-03-13 13:49:19 +0100

    moj commit

--- Vjezbe_1/pravac1.py
+++ Vjezbe_1/pravac1.py
@@ -1,15 +1,28 @@
-x1 = float(input("Unnesi x1 koordinatu 1. tocke: "))
-y1 = float(input("Unesite y1 koordinatu 2. tocke: "))
-x2 = float(input("Unesi x2 koordinatu 1. tocke: "))
-y2 = float(input("Unesite y2 koordinatu 2. tocke: "))
- 
-#ovako sam radio jer nisam radio funkciju pa sam isao direkt preko formule
+while True:
+    try:
+        x1 = float(input("Unnesi x1 koordinatu 1. tocke: "))
+        y1 = float(input("Unesite y1 koordinatu 2. tocke: "))
+        x2 = float(input("Unesi x2 koordinatu 1. tocke: "))
+        y2 = float(input("Unesite y2 koordinatu 2. tocke: "))
 
-a = y2 - y1
-b = x1 - x2
-c = a*x1 + b*y1
+        k = (y2 - y1) / (x2 - x1)
+        l = -k * x1 + y1
 
-if(b<0):
-    print("Jednadzba pravca je: ", a,"x",b,"y=",c)
-else:
-    print("Jednadzba pravca je: ", a,"x+",b,"y=",c)
+        n = x2 - x1
+
+        if(l<0):
+            if(n!= 0):
+                print("Jednadzba pravca je: y =",k,"x",l)
+            break
+        elif(l>0):
+            if(n!= 0):
+                print("Jednadzba pravca je: y =",k,"x +",l)
+            break
+        else:
+            if(n!= 0):
+                print("Jednadzba pravca je: y =",k,"x")
+            break
+    
+    except Exception as e:
+        print("Ponovi upis")
+    
