1086a9f36b509a3662a32b58d469b52ace99ef53
Drago <dmihaljev@pmfst.hr>
2022-04-08 11:54:10 +0200

    moj commit

--- Vjezbe_4/derivacija.py
+++ Vjezbe_4/derivacija.py
@@ -8,14 +8,15 @@ print(cal.value(funkcija, 0))
 print(cal.derivacija_u_tocki(funkcija, 0))
 
 x_os = np.linspace(-3.4,2.4,100)
-y_os = 6*x_os*x_os + 6*x_os + 4*x_os
-plt.plot(x_os, y_os)
+y_os = 6*x_os*x_os + 6*x_os + 4
+plt.plot(x_os, y_os, color = 'g')
 
 x, dfx = cal.derivacija_u_rasponu(funkcija, -3.4,2.4,0.1, metoda = 1)
+plt.scatter(x, dfx, s = 5, color = 'r')
 
 x, dfx = cal.derivacija_u_rasponu(funkcija,-3.4,2.4,0.01, metoda = 1)
+plt.scatter(x, dfx, s = 6, color = 'b')
 
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.scatter(x, dfx, s = 5, color = 'r', marker = 'o')
 plt.show()
