eb2ffaee6cf4acaf6d7cf0f651323652d97e632e
Drago <dmihaljev@pmfst.hr>
2022-04-22 11:19:53 +0200

    kolokvij

--- Kolokvij/konacni_program.py
+++ Kolokvij/konacni_program.py
@@ -1,5 +1,4 @@
 import Projectile_drop as pd
-import visina_brzina as vb
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,8 +8,8 @@ p0.init_cond(10, 20)
 print("Objekt je uspjesno stvoren")
 print("Pocetna brzina je v = {} m/s i pocetna visina je {} m".format(10, 20))
 
-p1 = vb.projectile_drop()
-print(p1.unos_v_h)
+h1 = input("Unesi visinu koju zelis: ")
+v1 = input("Uensi brzinu koju zelis: ")
 
 p2 = pd.projectile_drop()
 p2.init_cond(2000, 200, 0.01)
@@ -21,5 +20,3 @@ p3 = pd.projectile_drop()
 p1.init_cond(2000, 200)
 p3.vrijeme_trajanja_pada()
 x_os = np.linspace(0.001,0.1,100)
-y_os = p3.vrijeme_trajanja_pada*x_os
-plt.plot(x_os, y_os)
